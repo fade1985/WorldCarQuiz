669739bee47eb80e92a766bf3abda27e2c93b8dd
Sito <fade1985@gmail.com>
2014-03-20 11:46:48 +0100

    Subida gen

--- gen/com/android/worldcarquiz/R.java
+++ gen/com/android/worldcarquiz/R.java
@@ -22,20 +22,28 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int button_shape=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int shapes=0x7f020002;
+        public static final int button_circle_shape=0x7f020000;
+        public static final int button_shape=0x7f020001;
+        public static final int fondo=0x7f020002;
+        public static final int ic_action_split=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int ic_stats=0x7f020005;
+        public static final int shapes=0x7f020006;
     }
     public static final class id {
-        public static final int cars=0x7f070003;
+        public static final int awards_main_circle_button=0x7f070009;
+        public static final int cars=0x7f070004;
         public static final int container_list_worlds=0x7f070000;
         public static final int container_main_menu=0x7f070001;
-        public static final int image_list_worlds=0x7f070007;
-        public static final int play_main_menu_button=0x7f070004;
-        public static final int text_list_worlds=0x7f070008;
-        public static final int tutorial_main_menu_button=0x7f070006;
+        public static final int ic_action_split=0x7f070003;
+        public static final int image_list_worlds=0x7f07000b;
+        public static final int play_main_button=0x7f070005;
+        public static final int stamps_main_circle_button=0x7f07000a;
+        public static final int stats_main_circle_button=0x7f070008;
+        public static final int text_list_worlds=0x7f07000c;
+        public static final int tutorial_main_button=0x7f070007;
         public static final int world_quiz=0x7f070002;
-        public static final int worlds_main_menu_button=0x7f070005;
+        public static final int worlds_main_button=0x7f070006;
     }
     public static final class layout {
         public static final int container_list_worlds=0x7f030000;
