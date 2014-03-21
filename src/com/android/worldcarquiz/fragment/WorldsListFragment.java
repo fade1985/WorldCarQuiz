0e4923161de0eedff45a5900f866d789a57cc6c9
pablink84 <escarba@gmail.com>
2014-03-21 15:02:17 +0100

    Empezando con lista de mundos

--- src/com/android/worldcarquiz/fragment/WorldsListFragment.java
+++ src/com/android/worldcarquiz/fragment/WorldsListFragment.java
@@ -8,7 +8,7 @@ import android.support.v4.app.ListFragment;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -35,7 +35,7 @@ public class WorldsListFragment extends ListFragment {
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		ListView lv = getListView();
-		lv.setDividerHeight(20);
+		lv.setDividerHeight(0);
 		super.onActivityCreated(savedInstanceState);
 	}
 	
@@ -51,12 +51,14 @@ public class WorldsListFragment extends ListFragment {
 			if (convertView == null) {
 				convertView = getActivity().getLayoutInflater().inflate(R.layout.list_worlds_fragment, null);
 			}
+			//intento de convertir el color en un hexadecimal pero peta 
+			//int c = (int)Integer.parseInt("33B5E5", 16);
 			
 			int c = Color.BLUE;
 			c = c - (25 * position);	
 			
-			ImageView imageWorld = (ImageView)convertView.findViewById(R.id.image_list_worlds);
-			imageWorld.setBackgroundColor(c);
+			Button buttonWorld = (Button)convertView.findViewById(R.id.image_list_worlds);
+			buttonWorld.setBackgroundColor(c);
 			
 			TextView worldTitle = (TextView)convertView.findViewById(R.id.text_list_worlds);
 			worldTitle.setText("World" + String.valueOf(position));
