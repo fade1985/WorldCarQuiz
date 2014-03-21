c3a1980db25f80bba6b15babfa7d8ed7eb7ba180
pablink84 <escarba@gmail.com>
2014-03-21 13:23:25 +0100

    Fondos arreglados y fuentes

--- gen/com/android/worldcarquiz/R.java
+++ gen/com/android/worldcarquiz/R.java
@@ -23,22 +23,23 @@ public final class R {
     }
     public static final class drawable {
         public static final int button_circle_shape=0x7f020000;
-        public static final int button_shape=0x7f020001;
-        public static final int fondo=0x7f020002;
-        public static final int ic_action_favorite=0x7f020003;
-        public static final int ic_action_important=0x7f020004;
-        public static final int ic_action_network_cell=0x7f020005;
-        public static final int ic_action_split=0x7f020006;
-        public static final int ic_launcher=0x7f020007;
-        public static final int ic_stats=0x7f020008;
-        public static final int shapes=0x7f020009;
+        public static final int button_selector=0x7f020001;
+        public static final int button_shape=0x7f020002;
+        public static final int fondo=0x7f020003;
+        public static final int ic_action_favorite=0x7f020004;
+        public static final int ic_action_important=0x7f020005;
+        public static final int ic_action_network_cell=0x7f020006;
+        public static final int ic_action_split=0x7f020007;
+        public static final int ic_launcher=0x7f020008;
+        public static final int ic_stats=0x7f020009;
+        public static final int shapes=0x7f02000a;
     }
     public static final class id {
         public static final int awards_main_circle_button=0x7f070009;
-        public static final int cars=0x7f070004;
+        public static final int cars=0x7f070003;
         public static final int container_list_worlds=0x7f070000;
         public static final int container_main_menu=0x7f070001;
-        public static final int ic_action_split=0x7f070003;
+        public static final int ic_action_split=0x7f070004;
         public static final int image_list_worlds=0x7f07000b;
         public static final int play_main_button=0x7f070005;
         public static final int stamps_main_circle_button=0x7f07000a;
